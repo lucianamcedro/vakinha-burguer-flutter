51742aac15de7cb7139ed153ba55938361db1e41
Luciana Monteiro Cedro <luciana_cedro@hotmail.com>
2023-01-24 16:08:17 -0300

    feature/criação-endpoits-dio

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:vakinha_burguer_flutter/app/core/config/env/env.dart';
 import 'package:vakinha_burguer_flutter/app/dw9_delivery_app.dart';
 
-void main() {
+void main() async {
+  await Env.i.load();
   runApp(const Dw9DeliveryApp());
 }
