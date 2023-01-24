e00cb226881a82af579f505d57c7a87613612e86
Luciana Monteiro Cedro <luciana_cedro@hotmail.com>
2023-01-24 17:26:23 -0300

    feature/construção-splash-screen

--- lib/app/pages/splash/splash_page.dart
+++ lib/app/pages/splash/splash_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vakinha_burguer_flutter/app/core/ui/helpers/size_extensions.dart';
 import 'package:vakinha_burguer_flutter/app/core/ui/widgets/delivery_button.dart';
 
 class SplashPage extends StatelessWidget {
@@ -7,19 +8,43 @@ class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(''),
-      ),
-      body: Column(
+        body: ColoredBox(
+      color: const Color(0XFF140E0E),
+      child: Stack(
         children: [
-          Container(),
-          DeliveryButton(
-            label: 'Batata',
-            onPressed: () {},
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: SizedBox(
+              width: context.screenWidht,
+              child: Image.asset(
+                'assets/images/lanche.png',
+                fit: BoxFit.cover,
+              ),
+            ),
           ),
-          TextFormField(),
+          Center(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: context.percentHeight(.3),
+                ),
+                Image.asset(
+                  "assets/images/logo.png",
+                ),
+                const SizedBox(
+                  height: 80,
+                ),
+                DeliveryButton(
+                  width: context.percentWidth(.6),
+                  height: 35,
+                  label: 'ACESSAR',
+                  onPressed: () {},
+                )
+              ],
+            ),
+          ),
         ],
       ),
-    );
+    ));
   }
 }
