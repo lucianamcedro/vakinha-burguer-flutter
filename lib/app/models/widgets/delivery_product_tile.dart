a60ce34fe8b01b431f15d26e214970d5df551220
Luciana Monteiro Cedro <luciana_cedro@hotmail.com>
2023-01-26 15:51:50 -0300

    feature/criação-tela-home

---
+++ lib/app/models/widgets/delivery_product_tile.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:vakinha_burguer_flutter/app/core/extensions/formatter_extension.dart';
+import 'package:vakinha_burguer_flutter/app/core/ui/styles/colors_app.dart';
+import 'package:vakinha_burguer_flutter/app/core/ui/styles/text_styles.dart';
+
+import 'package:vakinha_burguer_flutter/app/models/product_model.dart';
+
+class DeliveryProductTile extends StatelessWidget {
+  final ProductModel productModel;
+
+  const DeliveryProductTile({
+    Key? key,
+    required this.productModel,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Row(
+        children: [
+          FadeInImage.assetNetwork(
+            placeholder: 'assets/images/loading.gif',
+            image: productModel.image,
+            width: 100,
+            height: 100,
+            fit: BoxFit.cover,
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: Text(
+                    productModel.name,
+                    style:
+                        context.textStyle.textExtraBold.copyWith(fontSize: 16),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: Text(
+                    productModel.description,
+                    style: context.textStyle.textLight.copyWith(fontSize: 12),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: Text(
+                    productModel.price.currencyPTBR,
+                    style: context.textStyle.textMedium.copyWith(
+                      fontSize: 11,
+                      color: context.colors.secondary,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:vakinha_burguer_flutter/app/core/ui/widgets/delivery_appbar.dart';
+import 'package:vakinha_burguer_flutter/app/models/product_model.dart';
+import 'package:vakinha_burguer_flutter/app/models/widgets/delivery_product_tile.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -6,10 +9,29 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(''),
+      appBar: DeliveryAppbar(),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: 5,
+              itemBuilder: (context, index) {
+                return DeliveryProductTile(
+                  productModel: ProductModel(
+                    id: 0,
+                    name: 'Lanche X',
+                    description:
+                        'Pão artesanal, com bife 200g, bacon, cebola caramelisada e maionese verde',
+                    price: 15,
+                    image:
+                        'https://th.bing.com/th/id/R.1dff5bd2b2428602c8bf3b6df939d58d?rik=J7Wbhlr7n%2fbHYw&riu=http%3a%2f%2filarge.lisimg.com%2fimage%2f5996523%2f740full-hamburger.jpg&ehk=ghWzrT9k4YLS1OGYngxrRgC%2fADev8H0szYvMpzlGIJw%3d&risl=&pid=ImgRaw&r=0',
+                  ),
+                );
+              },
+            ),
+          )
+        ],
       ),
-      body: Container(),
     );
   }
 }
