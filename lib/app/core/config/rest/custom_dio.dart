8059a3919dae6c614c98fc315d609fe8e6f3d4cf
Luciana Monteiro Cedro <luciana_cedro@hotmail.com>
2023-02-01 16:06:05 -0300

    feature/implementando-authinterceptors

--- lib/app/core/config/rest/custom_dio.dart
+++ lib/app/core/config/rest/custom_dio.dart
@@ -1,8 +1,10 @@
 import 'package:dio/dio.dart';
 import 'package:dio/native_imp.dart';
 import 'package:vakinha_burguer_flutter/app/core/config/env/env.dart';
+import 'package:vakinha_burguer_flutter/app/core/config/rest/auth_interceptor.dart';
 
 class CustomDio extends DioForNative {
+  late AuthInterceptor _authInterceptors;
   CustomDio()
       : super(BaseOptions(
           baseUrl: Env.i['backend_base_url'] ?? '',
@@ -11,12 +13,15 @@ class CustomDio extends DioForNative {
         )) {
     interceptors.add(LogInterceptor(
         requestBody: true, responseBody: true, requestHeader: true));
+    _authInterceptors = AuthInterceptor();
   }
   CustomDio auth() {
+    interceptors.add(_authInterceptors);
     return this;
   }
 
   CustomDio unAuth() {
+    interceptors.remove(_authInterceptors);
     return this;
   }
 }
